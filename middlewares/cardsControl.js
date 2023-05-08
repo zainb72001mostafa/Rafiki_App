d7d0911fddf486ec8c22d5dcb5198cf9a2e705d1
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-05-08 04:48:40 +0200

    third

--- middlewares/cardsControl.js
+++ middlewares/cardsControl.js
@@ -10,7 +10,7 @@ exports.storage = multer.diskStorage({
       cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname);
+      cb(null, Date.now().replace(/[\/\\:]/g, "-") + file.originalname);
     }
   });
   
